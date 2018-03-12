d5662bac05e8ff4d7c169a7aaf6f78174124ed03
Rafał Gajda <rafcio.gajda@gmail.com>
2018-03-12 15:02:35 +0100

    sms-js

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -68,4 +68,23 @@ options.desiredFields = [navigator.contacts.fieldType.id];
 options.hasPhoneNumber = true;
 var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
 
-	
+var app = {
+    sendSms: function() {
+        var number = document.getElementById('numberTxt').value.toString(); /* iOS: ensure number is actually a string */
+        var message = document.getElementById('messageTxt').value;
+        console.log("number=" + number + ", message= " + message);
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
+    }
+};	
